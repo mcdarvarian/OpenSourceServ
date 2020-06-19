11ab15bbbad5034e2b0169d790f7a00d7814dcd8
Jack <pupeljack@gmail.com>
2020-06-19 12:36:49 -0500

    set up basic endpoints

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const app = express();
+const bodyParser = express.json();
 
 const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
@@ -14,10 +15,18 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-
+//I would usually use a router for endpoints, but given theres only one im going to choose to not this time around
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+    res.send('Hello, world!');
+});
+
+app.post('/', bodyParser, (req, res) =>{
+    res.send('Hello, post!');
+});
+
+app.put('/', bodyParser, (req, res) =>{
+    res.send('Hello, put!');
+});
 
 app.use(function errorHandler(error, req, res, next) {
     let response
